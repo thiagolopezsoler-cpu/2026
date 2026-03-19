4b7a9c5ea4e4d8c0f899dec9c57848ce8fc05142
Thiagolopezsoler <thiagolopezsoler@gmail.com>
2026-03-19 14:19:45 -0300

    jdkd

--- 2026/python/variablesAndConditionals/ej9.py
+++ 2026/python/variablesAndConditionals/ej9.py
@@ -6,5 +6,6 @@ def validacion_fecha(fecha):
         print(f"La fecha {fecha_obj} es correcta")
     except ValueError:
         print("La fecha es incorrecta")
+        
 fecha = input("Ingrese la fecha(ej: 12/2/2000) ")
 validacion_fecha(fecha)
